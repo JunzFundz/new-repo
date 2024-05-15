bbca91938305936eee0a2787105a6ab6002d24da
Diongie Fundador <121327561+JunzFundz@users.noreply.github.com>
2024-05-16 07:15:54 +0800

    Update db_conn.php

--- database/db_conn.php
+++ database/db_conn.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect ('localhost', 'root', 'fundador142', 'inventory') or die('Unable to connect');
+$conn = mysqli_connect ('localhost', 'root', '', 'inventory') or die('Unable to connect');
 ?>
